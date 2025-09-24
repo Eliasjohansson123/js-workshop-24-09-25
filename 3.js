f0e79d55f068bfcf51650ce04c47206d4d887873
eliasjohansson123 <eliasjohansson9@gmail.com>
2025-09-24 12:12:20 +0200

    removing Elements

---
+++ 3.js
@@ -0,0 +1,18 @@
+
+
+// MÅL: Räkna ut slutbetyget utifrån provresultat och inlämnade projekt.
+// Tar emot en array och behåller det första elementet, sedan tar vi bort vartannat element från array.
+
+
+// INPUTS: Vilka variabler som kommer till funktionen och vilken typ de är
+// en array med strängar.
+
+
+
+// RETURN: Vad ska skickas tillbaka efter beräkning eller behandling av
+// Varannan sträng i arrayen.
+
+// Funktionsbeskrivning
+// Deklarera en placeholder array. 
+// Kolla om elementens index mod 2 = 0. Om true pusha item till placeholder array, om false gör ingenting.
+// returnera placeholder array. 
