8dc5ccad6bca2abb2adc1e91437baa4adea86843
eliasjohansson123 <eliasjohansson9@gmail.com>
2025-09-24 10:37:46 +0200

    pseudokod

---
+++ script.js
@@ -0,0 +1,6 @@
+// Skapa en funktion som heter "trimSpaces" med ett argument som är en sträng som innehåller whitespaces som du vill ta bort.
+// inuti trimSpaces, skapa en variabel som innehåller den text du vill ta bort tomma tecken ifrån.
+// Låt variabeln vara en "let" variabel.
+// "trimmed" variabeln skall vara lika med den första variabeln som skapades men avslutas med string.replaceAll(pattern, replacement),
+// där pattern bestäms av regex som väljer alla whitespaces i variabeln "trimmed".                              /\s/g  ,    ""
+// returnera "let" variabeln, som kallas "trimmed".
